f2036b7eee31663616f64196b6f1f3450b289288
Devam Patel <devampatelcode@gmail.com>
2024-07-23 00:08:14 +0530

    Revert "--"
    This reverts commit 3c50f77a5c91e5f3ebcc3a198a52c193fd2b6b2e.

--- src/main.py
+++ src/main.py
@@ -24,14 +24,14 @@ def get_grayscale(image):
 
 def remove_noise(image):
     return cv2.medianBlur(image, 5)
-
+ 
 def thresholding(image):
     return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
 def dilate(image):
     kernel = np.ones((5, 5), np.uint8)
     return cv2.dilate(image, kernel, iterations=1)
-
+    
 def erode(image):
     kernel = np.ones((5, 5), np.uint8)
     return cv2.erode(image, kernel, iterations=1)
