75e16756e164446de86f993ce47279fc23f2af03
Devam Patel <devampatelcode@gmail.com>
2024-07-23 12:37:26 +0530

    --

--- src/main.py
+++ src/main.py
@@ -166,11 +166,11 @@ def main(context):
         return context.res.json({"result": result})
 
     # `ctx.res.json()` is a handy helper for sending JSON
-    # return context.res.json(
-    #     {
-    #         "motto": "Build like a team of hundreds_",
-    #         "learn": "https://appwrite.io/docs",
-    #         "connect": "https://appwrite.io/discord",
-    #         "getInspired": "https://builtwith.appwrite.io",
-    #     }
-    # )
+    return context.res.json(
+        {
+            "motto": "Build like a team of hundreds_",
+            "learn": "https://appwrite.io/docs",
+            "connect": "https://appwrite.io/discord",
+            "getInspired": "https://builtwith.appwrite.io",
+        }
+    )
